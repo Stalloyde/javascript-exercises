1fb3722fb5ba9e6a1ce30ce73f7f06bf1b1ef37e
Stalloyde <stalloyde93@gmail.com>
2022-05-25 15:03:20 +0800

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let string = '';
 
-};
-
+    const splitString = word.split("");
+    const reverseArray = splitString.reverse();
+    string = reverseArray.join("");
+    return string;
+}
 // Do not edit below this line
 module.exports = reverseString;
+
+
+/*
+passing parameter of word to callback function
+convert parameter letters to array
+reverse array
+convert array to string
+assign reverse array to variable string
+return string
+
+*/
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arg) {
 
-};
-
+    const array = arg.shift();
+    let newArray = [];
+    
+    outer: for (let i = 0; i < array.length; i++) {
+      for (let x = 0; x < arg.length; x++) {
+        if (array[i] === arg[x]) continue outer;
+        } newArray.push(array[i]);
+      } return newArray;
+    };
+  
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
