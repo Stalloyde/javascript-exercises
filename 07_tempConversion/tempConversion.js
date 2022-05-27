289afa1db036b4720f207da2530f9b76d1312542
Stalloyde <stalloyde93@gmail.com>
2022-05-27 10:53:00 +0800

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fInput) {
+  let getCelsius = (fInput -32) * 5/9;
+  
+    return parseFloat(getCelsius.toFixed(1));
+  }
 
-};
-
-const ctof = function() {
-
-};
+const ctof = function(cInput) {
+  let getFahrenheit = (cInput * 9/5) + 32;
+  
+      return parseFloat(getFahrenheit.toFixed(1));
+    }
 
 // Do not edit below this line
 module.exports = {
